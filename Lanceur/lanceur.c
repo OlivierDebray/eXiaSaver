d19b4d5667a766f17401df8ebc3a7e46b29a5607
OlivierDebray <olivier.debray@viacesi.fr>
2016-12-12 12:05:36 +0100

    MAJ 12/12 12:00

--- Lanceur/lanceur.c
+++ Lanceur/lanceur.c
@@ -2,23 +2,57 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
+
+void stats(int type , int param) ;
 
 void lancementStatique()
 {
 	printf("Lancement du TermSaver statique\n") ;
 	sleep(1) ;
+
+        int rand ;
+        FILE *f = fopen("/dev/urandom" , "r") ;
+        fread(&rand , 1 , 1 , f) ;
+        fclose(f) ;
+
+	int choix = 0 ;
+
+	int nb_img = 8 ;	// Compléter avec les directives de répertoires
+	int tmp = (256 / nb_img) ;
+
+	int i = 0 ;
+
+	while (choix == 0)
+	{
+		if (rand < (tmp * i))
+			choix = i ;
+		i++ ;
+	}
+
+	stats(1 , choix) ;
+
+	printf("choix %d\n" , choix) ;
 }
 
 void lancementDynamique()
 {
         printf("Lancement du TermSaver dynamique\n") ;
         sleep(1) ;
+
+	int taille ;
+
+	stats(2 , taille) ;
 }
 
 void lancementInteractif()
 {
         printf("Lancement du TermSaver interactif\n") ;
         sleep(1) ;
+
+	int pos ;
+
+	stats(3 , pos) ;
 }
 
 void lancementStatistiques()
@@ -29,29 +63,46 @@ void lancementStatistiques()
 
 void lancementAleatoire()
 {
-	while (1)
+       	int rand ;
+
+        FILE *f = fopen("/dev/urandom" , "r") ;
+       	fread(&rand , 1 , 1 , f) ;
+       	fclose(f) ;
+
+	switch ((rand % 3) + 1)
 	{
-		int rand ;
-		FILE *f ;
-
-		f = fopen("/dev/urandom" , "r") ;
-		fread(&rand , 1 , 1 , f) ;
-		fclose(f) ;
-
-		if (rand < 85)
-		{
+		case 1 :
 			lancementStatique() ;
 			break ;
-		}
-		else if (rand < 170)
-		{
+		case 2 :
 			lancementDynamique() ;
 			break ;
-		}
-		else if (rand < 255)
-		{
+		case 3 :
 			lancementInteractif() ;
 			break ;
-		}
 	}
+}
+
+void stats(int type , int param)
+{
+	time_t t = time(NULL) ;
+
+	FILE* stats = fopen("/mnt/hgfs/eXiaSaver/stats.txt" , "a") ;
+
+	switch (type)
+	{
+		case 1 :
+			fprintf(stats , "Statique     |") ;
+			break ;
+                case 2 :
+                        fprintf(stats , "Dynamique    |") ;
+			break ;
+                case 3 :
+                        fprintf(stats , "Interactif   |") ;
+			break ;
+	}
+
+	fprintf(stats , "     %d     |" , param) ;
+
+        fprintf(stats , "     %s" , asctime(localtime(&t))) ;
 }
--- Lanceur/main.c
+++ Lanceur/main.c
@@ -8,25 +8,25 @@ void main (int argc , char **argv)	// Fonction principale dans laquelle on récu
 {
 	system("clear") ;	// Nettoyage de la console
 
-	if (argc > 1)
+	if (argc > 1)	// On teste si l'utilisateur a passé un argument avec la commande
 	{
-		if (strcmp(argv[1] , "-1") == 0)
+		if (strcmp(argv[1] , "-1") == 0)	// Si l'argument est -1 on lance le TermSaver Statique
 			lancementStatique() ;
 
-                else if (strcmp(argv[1] , "-2") == 0)
+                else if (strcmp(argv[1] , "-2") == 0)	// S'il est -2 on lance le TermSaver Dynamique
                         lancementDynamique() ;
 
-                else if (strcmp(argv[1] , "-3") == 0)
+                else if (strcmp(argv[1] , "-3") == 0)	// S'il est -3 on lance le TermSaver Interactif
                         lancementInteractif() ;
 
-                else if(strcmp(argv[1] , "-stats") == 0)
+                else if(strcmp(argv[1] , "-stats") == 0)	// S'il est -stats on afficher les statistiques
                         lancementStatistiques() ;
 
 		else
-			lancementAleatoire() ;
+			lancementAleatoire() ;	// Si l'argument est tout autre, on lance un TermSaver aléatoire
 	}
 	else
 	{
-		lancementAleatoire() ;
+		lancementAleatoire() ;	// De même, s'il n'y a pas de paramètres, on lance un TermSaver aléatoire
 	}
 }
